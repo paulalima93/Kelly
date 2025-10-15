840c4f4b5967b3e83ef2abd20177025bf8e484c0
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2025-10-15 15:05:02 -0300

    Exercicios 04 - LOOPS

---
+++ Exercicios/04.js
@@ -0,0 +1,17 @@
+// ----------------------------------------
+// 🧩 Exercícios de fixação - LOOPS
+// ----------------------------------------
+
+//1️ Crie um loop for que mostre os números de 1 a 10.
+
+//2️ Faça um loop while que conte de 10 até 1.
+
+//3️ Crie um loop que exiba apenas os números pares de 1 a 20.
+
+//4️ Use um for...of para mostrar todos os nomes de um array de alunos.
+
+//5️ Crie um loop que calcule a soma dos números de 1 a 100.
+
+//6️ Faça um loop que percorra um array de notas e mostre apenas as maiores ou iguais a 7.
+
+//7️ Use um for...in para exibir todas as propriedades e valores de um objeto "carro".
